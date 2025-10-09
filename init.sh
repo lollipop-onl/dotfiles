1d15b9f15de57a79e615ea1f2ecfee7157eb796e
Ryoya Tamura <ryoya.tamura@nulab.com>
2025-10-09 23:40:44 +0900

    fix syntax

--- init.sh
+++ init.sh
@@ -14,7 +14,7 @@ find "$DOTFILES_DIR" -type f -print0 | while IFS= read -r -d $'\0' source_path; 
   target_path="$TARGET_DIR/$relative_path"
   target_parent_dir="${target_path:h}"
 
-  if [! -e "$target_parent_dir" ]; then
+  if [ ! -d "$target_parent_dir" ]; then
     echo "Creating directory: $target_parent_dir"
     mkdir -p "$target_parent_dir"
   fi
