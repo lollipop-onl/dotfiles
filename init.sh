0238b8282c181e3d6510b2332c3bf658c2ace7ba
Ryoya Tamura <ryoya.tamura@nulab.com>
2025-10-10 20:01:12 +0900

    update

--- init.sh
+++ init.sh
@@ -1,13 +1,37 @@
 #!/bin/zsh
 
-./scripts/setup_symlinks.sh
+# configs ディレクトリのファイルをホームディレクトリにシンボリックリンクとして配置する
+DOTFILES_DIR=$HOME/dotfiles/configs
+TARGET_DIR=$HOME
 
-source ~/.zshrc
+echo "--- DIRECTORY SETUP AND SYMLINK CREATION ---"
+
+find "$DOTFILES_DIR" -type f -print0 | while IFS= read -r -d $'\0' source_path; do
+  relative_path=${source_path#"$DOTFILES_DIR/"}
+  target_path="$TARGET_DIR/$relative_path"
+  target_parent_dir="${target_path:h}"
+
+  if [ ! -d "$target_parent_dir" ]; then
+    echo "Creating directory: $target_parent_dir"
+    mkdir -p "$target_parent_dir"
+  fi
+
+  echo "Linking: $source_path -> $target_path"
+  ln -sf "$source_path" "$target_path"
+done
+
+echo "DONE."
+
+source ~/.config/zsh/.zshrc
 
 # 必要なツールのインストール
 # mise-en-place
-curl -fsSL https://mise.run | sh
+if [ ! -f ~/.local/bin/mise ]; then
+  curl -fsSL https://mise.run | sh
+fi
 ~/.local/bin/mise install
 
 # oh-my-zsh のインストール
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
