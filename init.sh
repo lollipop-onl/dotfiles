b70e55f4647be5f463bf4786c20b489d2bea0c7d
Ryoya Tamura <ryoya.tamura@nulab.com>
2025-10-11 02:40:37 +0900

    chore: sync dotfiles - 2025-10-11 02:40:37

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 DOTFILES_DIR=$HOME/dotfiles/configs
 TARGET_DIR=$HOME
 
-echo "--- DIRECTORY SETUP AND SYMLINK CREATION ---"
+echo "[symlink] Linking dotfiles from $DOTFILES_DIR to $TARGET_DIR..."
 
 find "$DOTFILES_DIR" -type f -print0 | while IFS= read -r -d $'\0' source_path; do
   relative_path=${source_path#"$DOTFILES_DIR/"}
@@ -20,16 +20,20 @@ find "$DOTFILES_DIR" -type f -print0 | while IFS= read -r -d $'\0' source_path; 
   ln -sf "$source_path" "$target_path"
 done
 
-echo "DONE."
+echo "[symlink] Dotfiles linking completed."
 
 # 必要なツールのインストール
 # mise-en-place
 if [ ! -f ~/.local/bin/mise ]; then
   curl -fsSL https://mise.run | sh
+else
+  echo "[skip] mise is already installed."
 fi
 ~/.local/bin/mise install
 
 # oh-my-zsh のインストール
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo "[skip] oh-my-zsh is already installed."
 fi
